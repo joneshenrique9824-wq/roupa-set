34e6cfcae752fe3fe4a88d173bf69d62076ac644
joneshenrique9824-wq <joneshenrique9824@gmail.com>
2026-05-06 09:08:32 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+import "dotenv/config";
+
+export const config = {
+  TOKEN: process.env.TOKEN,
+  CLIENT_ID: process.env.CLIENT_ID,
+  GUILD_ID: process.env.GUILD_ID,
+  META_CATEGORIA: process.env.META_CATEGORIA
+};
