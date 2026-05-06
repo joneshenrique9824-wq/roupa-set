c5c2c2a756ec18cfd73d97be38a9bc9a5ca0191a
joneshenrique9824-wq <joneshenrique9824@gmail.com>
2026-05-06 09:29:05 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,14 @@ export const config = {
   TOKEN: process.env.TOKEN,
   CLIENT_ID: process.env.CLIENT_ID,
   GUILD_ID: process.env.GUILD_ID,
-  META_CATEGORIA: process.env.META_CATEGORIA
+
+  CANAL_MASCULINO: process.env.CANAL_MASCULINO,
+  CANAL_FEMININO: process.env.CANAL_FEMININO,
+
+  META_CATEGORIA: process.env.META_CATEGORIA,
+  META_CANAL: process.env.META_CANAL,
+
+  CARGO_MEMBRO: process.env.CARGO_MEMBRO,
+  CARGO_LIDER: process.env.CARGO_LIDER,
+  CARGO_GERENTE: process.env.CARGO_GERENTE
 };
